7222f6e478caf2a6ac3d92dffde042b14df041fc
EE96 <elijahrae@outlook.com>
2022-08-05 14:40:25 +0100

    refactored client stuff, used aws own solutions instead of jerryrigged stuff

--- backend/dynamo/ChargeDeviceClient.ts
+++ backend/dynamo/ChargeDeviceClient.ts
@@ -1,94 +1,22 @@
-import { GetItemCommand, GetItemCommandInput, PutItemCommand, PutItemCommandInput } from "@aws-sdk/client-dynamodb";
-
+import { ChargeDevice } from "../types/ChargeDevice";
 import { DynamoClient } from "./DynamoClient";
-import {
-    ChargeDevice,
-    Connector,
-    ConnectorChargeStatus,
-    DynamoChargeDevice,
-    DynamoConnector
-} from "../types/ChargeDevice";
 
 export default {
-    put: async (item: ChargeDevice) => {
-        const params: PutItemCommandInput = {
+    put: async (Item: ChargeDevice) => {
+        return await DynamoClient.put({
             TableName: "ChargeDevices",
-            Item: chargeDeviceToDynamo(item)
-        };
-        return await DynamoClient.send(new PutItemCommand(params));
+            Item
+        });
     },
+
     fetch: async (chargeDeviceId: string) => {
-        const params: GetItemCommandInput = {
+        const data = await DynamoClient.get({
             TableName: "ChargeDevices",
-            Key: { ChargeDeviceId: { S: chargeDeviceId } }
-        };
-        const data = await DynamoClient.send(new GetItemCommand(params));
+            Key: { ChargeDeviceId: chargeDeviceId } 
+        });
         if (!data?.Item) {
             return null;
         }
-        const chargeDevice = dynamoToChargeDevice(data.Item as DynamoChargeDevice)
-        return chargeDevice;
+        return data.Item as ChargeDevice;
     },
 }
-
-
-function chargeDeviceToDynamo(chargeDevice: ChargeDevice): DynamoChargeDevice {
-    const dynamoDevice: DynamoChargeDevice = {
-        ChargeDeviceId: { S: chargeDevice.ChargeDeviceId },
-        ChargeDeviceName: { S: chargeDevice.ChargeDeviceName },
-
-        ChargeDeviceCoordinates: {
-            M:
-            {
-                Latitude: { N: chargeDevice.ChargeDeviceCoordinates.Latitude.toString() },
-                Longitude: { N: chargeDevice.ChargeDeviceCoordinates.Longitude.toString() }
-            }
-        },
-        Connectors: {
-            L: chargeDevice.Connectors.map(connectorToDynamo)
-        },
-        Charges: { N: chargeDevice.Charges.toString() }
-    };
-
-    if (chargeDevice.ChargeDeviceShortDescription) {
-        dynamoDevice.ChargeDeviceShortDescription = { S: chargeDevice.ChargeDeviceShortDescription };
-    }
-
-    return dynamoDevice;
-}
-
-function dynamoToChargeDevice(dynamoChargeDevice: DynamoChargeDevice): ChargeDevice {
-    return {
-        ChargeDeviceId: dynamoChargeDevice.ChargeDeviceId.S,
-        Charges: parseInt(dynamoChargeDevice.Charges.N),
-        ChargeDeviceName: dynamoChargeDevice.ChargeDeviceName.S,
-        ChargeDeviceShortDescription: dynamoChargeDevice.ChargeDeviceShortDescription?.S ?? null,
-        ChargeDeviceCoordinates: {
-            Latitude: parseInt(dynamoChargeDevice.ChargeDeviceCoordinates.M.Latitude.N),
-            Longitude: parseInt(dynamoChargeDevice.ChargeDeviceCoordinates.M.Longitude.N)
-        },
-        Connectors: dynamoChargeDevice.Connectors.L.map(dynamoChargeDevice => dynamoToConnector(dynamoChargeDevice))
-    }
-}
-
-
-function connectorToDynamo(connector: Connector): DynamoConnector {
-    return {
-        M: {
-            ConnectorId: { S: `${connector.ConnectorId}` },
-            RatedOutputkW: { N: connector.RatedOutputkW.toString() },
-            ChargePointStatus: { S: connector.ChargePointStatus }
-        }
-    }
-}
-
-function dynamoToConnector(dynamoConnector: DynamoConnector): Connector {
-    return {
-        ConnectorId: dynamoConnector.M.ConnectorId.S,
-        RatedOutputkW: parseInt(dynamoConnector.M.RatedOutputkW.N),
-        ChargePointStatus: dynamoConnector.M.ChargePointStatus.S as ConnectorChargeStatus
-    }
-}
-
-
-
--- backend/dynamo/DynamoClient.ts
+++ backend/dynamo/DynamoClient.ts
@@ -1,5 +1,6 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
 
 const REGION = "eu-west-2"; //e.g. "us-east-1"
 
-export const DynamoClient = new DynamoDBClient({ region: REGION });
+export const DynamoClient = DynamoDBDocument.from(new DynamoDB({ region: REGION }));
